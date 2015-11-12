d6b416c78b5dca46904ffbd2466ba0039ff02e9a
Trần Trung Hậu <trunghau.t1@gmail.com>
2015-11-13 02:00:46 +0700

    Update DatePicker

---
+++ FlatTheme/Code/CustomPopupPlacementCallbackHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace FlatTheme.Code
+{
+    public static class CustomPopupPlacementCallbackHelper
+    {
+        public static readonly CustomPopupPlacementCallback LargePopupCallback;
+
+        static CustomPopupPlacementCallbackHelper()
+        {
+            LargePopupCallback =
+                (size, targetSize, offset) => new[] { new CustomPopupPlacement(new Point(), PopupPrimaryAxis.Horizontal) };
+        }
+    }
+}
---
+++ FlatTheme/Code/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace FlatTheme.Code
+{
+    internal static class StringExtensions
+    {
+        public static string ToTitleCase(this string text, string separator = " ")
+        {
+            TextInfo textInfo = CultureInfo.CurrentUICulture.TextInfo;
+
+            string lowerText = textInfo.ToLower(text);
+            string[] words = lowerText.Split(new[] { separator }, StringSplitOptions.None);
+
+            return String.Join(separator, words.Select(v => textInfo.ToTitleCase(v)));
+        }
+    }
+}
